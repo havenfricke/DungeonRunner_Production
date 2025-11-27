8a8863a66b90ae9186b51e8d8352d62ede9bb877
wmullani <156372376+wmullani@users.noreply.github.com>
2025-11-27 13:19:55 -0700

    Fixed Death Logic and Player Damage
    Adjusted the PlayerHealth script and the damage value in the player prefab.

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -48,6 +48,8 @@ public class PlayerHealth : MonoBehaviour
 
         Debug.Log($"{name} has died!");
 
+        GameManager.Instance.PlayerDied(this);
+
         // Wait 10 seconds
         yield return new WaitForSeconds(10f);
 
